8d556b94a07d79f7bd9c779b7183bf3f17095248
Igor-Koniukhov <ikoniukhov@gmail.com>
2024-08-11 12:23:47 +0300

    Add logic for mail creation

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "8082"
 
 func main() {
 
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Started mail service")
 	srv := &http.Server{
@@ -26,4 +31,19 @@ func main() {
 		log.Panicln(err)
 	}
 
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+	return m
 }
