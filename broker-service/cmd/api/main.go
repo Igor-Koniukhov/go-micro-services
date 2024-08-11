5a4209f8ad6e2ff7a8f1ea5c1fa3aaa5ae23e132
Igor-Koniukhov <ikoniukhov@gmail.com>
2024-08-11 18:37:33 +0300

    Add rabbit connection logic

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
 )
 
 const webPort = "8082"
 
-type Config struct{}
+type Config struct {
+	Rabbit *amqp.Connection
+}
 
 func main() {
-	app := Config{}
+	rabbitcon, err := connect()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer rabbitcon.Close()
+	app := Config{
+		Rabbit: rabbitcon,
+	}
 
 	log.Printf("Starting broker service on port %s\n", webPort)
 
@@ -22,8 +36,35 @@ func main() {
 	}
 
 	// start the server
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			return nil, err
+			counts++
+		} else {
+			connection = c
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+
+		}
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off")
+		time.Sleep(backOff)
+
+	}
+	return connection, nil
 }
--- listener-service/main.go
+++ listener-service/main.go
@@ -1,1 +1,60 @@
-package listener_service
+package main
+
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"listener/event"
+	"log"
+	"math"
+	"os"
+	"time"
+)
+
+func main() {
+	rabbitcon, err := connect()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer rabbitcon.Close()
+
+	fmt.Println("Listening for and consuming RabbitMQ messages...")
+
+	consumer, err := event.NewConsumer(rabbitcon)
+	if err != nil {
+		panic(err)
+	}
+
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			return nil, err
+			counts++
+		} else {
+			connection = c
+			break
+		}
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+
+		}
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off")
+		time.Sleep(backOff)
+
+	}
+	return connection, nil
+}
